b2977366f9d0dad0113816862b9d8473319ddaaf
nepile <nevillejro.laia@gmail.com>
2023-07-29 01:58:29 +0700

    Setup report presensi

--- assets/styles/ReportStyle.js
+++ assets/styles/ReportStyle.js
@@ -1,3 +1,23 @@
 import { StyleSheet } from "react-native";
+import { _width } from "../../utils/Dimenssion";
 
-export const styles = StyleSheet.create([]);
+export const styles = StyleSheet.create({
+  cardContainer: {
+    width: _width,
+    paddingHorizontal: 15,
+  },
+  card: {
+    backgroundColor: "#fff",
+    elevation: 2,
+    borderRadius: 5,
+    padding: 15,
+  },
+  menu: {
+    marginTop: 20,
+    borderBottomColor: "#eee",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+});
--- views/core/Report.js
+++ views/core/Report.js
@@ -1,14 +1,45 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, TouchableOpacity, Image } from "react-native";
 import { styles } from "../../assets/styles/ReportStyle";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Report({ navigation }) {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Ini Halaman Login</Text>
-      </View>
+      <ScrollView>
+        <View style={styles.cardContainer}>
+          <View style={styles.card}>
+            <TouchableOpacity style={[styles.menu, { marginTop: 5 }]}>
+              <Text style={{ marginBottom: 10, fontSize: 17 }}>Hari ini</Text>
+              <View style={{ width: 10, height: 10 }}>
+                <Image
+                  source={require("../../assets/arrow.png")}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.menu}>
+              <Text style={{ marginBottom: 10, fontSize: 17 }}>
+                Rekapan Presensi
+              </Text>
+              <View style={{ width: 10, height: 10 }}>
+                <Image
+                  source={require("../../assets/arrow.png")}
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    resizeMode: "contain",
+                  }}
+                />
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
