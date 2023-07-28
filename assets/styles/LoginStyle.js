e5ecec14370807515889a7c467a74a23ced1695b
nepile <nevillejro.laia@gmail.com>
2023-07-29 02:26:00 +0700

    Setup login view

--- assets/styles/LoginStyle.js
+++ assets/styles/LoginStyle.js
@@ -1,3 +1,37 @@
 import { StyleSheet } from "react-native";
+import _width, { _height } from "../../utils/Dimenssion";
 
-export const styles = StyleSheet.create([]);
+export const styles = StyleSheet.create({
+  container: {
+    width: _width,
+    height: _height,
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  formContainer: {
+    marginTop: "30%",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+    marginTop: 10,
+    width: 250,
+    padding: 5,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+  },
+  btnLogin: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#AF1F22",
+    borderRadius: 10,
+    marginTop: 20,
+    paddingVertical: 15,
+  },
+  textBtn: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#fff",
+  },
+});
--- views/auth/Login.js
+++ views/auth/Login.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "../../assets/styles/LoginStyle";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Login({ navigation }) {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Ini Halaman Login</Text>
+      <View style={styles.container}>
+        <Image
+          source={require("../../assets/logo.png")}
+          style={{ width: 100, height: 100, marginTop: 20 }}
+        />
+
+        <View style={styles.formContainer}>
+          <Text style={{ fontWeight: "bold" }}>Login into Your Account</Text>
+          <TextInput style={styles.textInput} placeholder="NIP/NIS" />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Password"
+            secureTextEntry
+          />
+          <TouchableOpacity style={styles.btnLogin}>
+            <Text style={styles.textBtn}>Login</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
